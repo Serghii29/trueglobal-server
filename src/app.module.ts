cfa4b1ef7e6c14550869eb133526e312dd618274
Tretiak Serhii <tretyak8936@gmail.com>
2023-07-03 23:01:47 +0300

    set up docker, database and prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,19 @@ import { UserModule } from './user/user.module';
 import { CategoriesModule } from './categories/categories.module';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, CategoriesModule, TasksModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    CategoriesModule,
+    TasksModule,
+    AuthModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
