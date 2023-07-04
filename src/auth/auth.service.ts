3790035c66dfceef2639aa8ad6409d371fd4a0cc
Tretiak Serhii <tretyak8936@gmail.com>
2023-07-04 19:08:41 +0300

    set up jwt, guard, auth and user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,30 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { JwtService } from '@nestjs/jwt';
+import * as argon from 'argon2';
+import { IUser } from 'src/types/types';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.getOneOrFail(email);
+    const passwordIsMatch = await argon.verify(user.hashed_password, password);
+
+    return passwordIsMatch ? user : new Error('Password is incorect!');
   }
 
-  findAll() {
-    return 'This action returns all auth';
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async login(user: IUser) {
+    const { id, email } = user;
+    const payload = { id, email };
+    return {
+      id,
+      email,
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import * as argon from 'argon2';
+import { User } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const hashed_password = await argon.hash(createUserDto.password);
+
+      const existUser = await this.prisma.user.findUnique({
+        where: {
+          email: createUserDto.email,
+        },
+      });
+
+      if (existUser) {
+        throw new BadRequestException('This email already exist');
+      }
+
+      const user = await this.prisma.user.create({
+        data: {
+          email: createUserDto.email,
+          hashed_password,
+          name: createUserDto.name,
+        },
+      });
+
+      const token = this.jwtService.sign({ email: createUserDto.email });
+
+      return { user, token };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credential taken');
+        }
+      }
+      throw error;
+    }
   }
 
-  findAll() {
-    return 'This action returns all user';
+  async findOne(email: string): Promise<User | null> {
+    return await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+  async getOneOrFail(email: string): Promise<User> {
+    const user = await this.findOne(email);
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+    if (!user) {
+      throw new Error('User not found');
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+    return user;
   }
 }
